62d161dd0fc907204f053f1458dbbcf1ba7ec17d
nicolasgdev <contactnicolasgarcia@gmail.com>
2024-04-16 20:12:53 -0300

    changed name port -> PORT

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT
+const PORT = process.env.PORT
 
 app.use('/', (req, res) => {
   res.send('Hello there!')
 })
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`) 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`) 
 })
