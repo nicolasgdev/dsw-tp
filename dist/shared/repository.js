0590798df26b66198c2d6f94bf47dd4a65b41555
Valentín Josué Baldomá <vbaldoma@frro.utn.edu.ar>
2024-06-27 17:28:10 -0300

    Add files via upload

---
+++ dist/shared/repository.js
@@ -0,0 +1,3 @@
+// ORM -> Patrón Data Mapper -> Robusto, escalabre, sencillo de modificar y separa los conceptos de BE y DBA en dos componentes (clases) diferentes.
+export {};
+//# sourceMappingURL=repository.js.map
---
+++ dist/user/user.entity.js
@@ -0,0 +1,10 @@
+export class User {
+    constructor(id, dni, name, surname, email) {
+        this.id = id;
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+}
+//# sourceMappingURL=user.entity.js.map
